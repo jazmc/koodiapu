3450806f5f343d48e15160cbf85c9e9807e54e51
Jasmin Lumme <82147609+jazmc@users.noreply.github.com>
2023-08-30 13:04:17 +0300

    Update and rename bam.php to tasotJaPisteetTekstinaAllekkain2.php

---
+++ tasotJaPisteetTekstinaAllekkain2.php
@@ -0,0 +1,57 @@
+<!-- tämä sivun alkuun ennen < !doctype... hässäkkää -->
+
+<?php
+//Muokkaa tähän hevosesi VH-tunnus
+$vh = 'VH19-053-0048';
+
+//Muokkaa tähän hevosen PORRASTETTUJEN painotuslajit
+$osaaminen = array("KRJ", "KERJ");
+
+// ei tarvittavia muokkauksia paitsi jos tulee joku uusi laji, laitoin nämä neljä esimerkiksi
+$taivutus = array("VVJ" => "Valjakkoajo", "KRJ" => "Kouluratsastus", "ERJ" => "Esteratsastus", "KERJ" => "Kenttäratsastus");
+$url = 'http://virtuaalihevoset.net/rajapinta/porrastetut/' . $vh;
+$obj = json_decode(file_get_contents($url), true);
+
+if (isset($obj['error']) && $obj['error'] == 0) {
+    $data = $obj['porrastetut'];
+    $info = $data['info'];
+    $hevonen = $data['hevonen'];
+}
+?>
+
+
+<!-- tämä siihen kohtaan mihin haluat että kaikkien porrastettujen jaosten jutut tulostuu -->
+<?php
+// jos hevonen on haettu epäonnistuneesti, tulosta virhe
+if ($hevonen['error'] == 1) {
+    echo $hevonen['error_message'];
+} else {
+    foreach ($hevonen['tasot'] as $jaos => $tasoinfo) {
+        // summan alustus jaoksen ominaisuuspisteille
+        $summa = 0;
+
+        // haetaan tiedot rajapinnasta muuttujiin
+        $jaosnimi = $info['jaokset'][$jaos]['jaos_nimi'];
+        $jaoslyhenne = $info['jaokset'][$jaos]['jaos_lyhenne'];
+        $jaoksenominaisuudet = $info['jaokset'][$jaos]['ominaisuudet'];
+        $taso = $tasoinfo['taso'];
+        $max_taso_rajoitus = $tasoinfo['taso_rajoitus'];
+
+        // loopataan jokaisen jaoksen ominaisuuden läpi
+        foreach ($jaoksenominaisuudet as $id) {
+            $ominaisuusnimi = $info['ominaisuudet'][$id];
+            $ominaisuuspisteet = $hevonen['ominaisuudet'][$id]['pisteet'];
+
+            //summaa ominaisuuden pisteet summamuuttujaan
+            $summa += $ominaisuuspisteet;
+        }
+
+        //Tulostus, jos jaos kuuluu hevosen osaamiseen
+        if (in_array($jaoslyhenne, $osaaminen)) {
+            echo $taivutus[$jaoslyhenne] . "<br>";
+            echo "&#8618; tasolla <b>" . $taso . "/" . $max_taso_rajoitus . "</b> (" . $summa . " p.)<br>";
+        }
+    }
+}
+
+?>
